432c0aa0904a54ffb7772c003e8523fe32e23cbd
o0cheska0o <109116214+o0cheska0o@users.noreply.github.com>
2024-04-07 21:57:28 -0700

    test 1

--- run.py
+++ run.py
@@ -3,6 +3,6 @@ from app import app, db
 if __name__ == '__main__':
     print("Above if means, I am being run directly (inside run.py), not as an import from another module")
     with app.app_context():
-        # Create the database and tables
+        # Create the database and tables WEeee
         db.create_all()
     app.run(debug=True)
